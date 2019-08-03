5d324c1f70e2e27280b090975bcb1ac4f96f7300
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2019-08-03 10:48:25 +0530

    basic layout of homepage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>-->
+      <div class="logo">
+        <img
+          data-v-64fe992e
+          src="https://app.incento.io/_nuxt/img/5d8e998.svg"
+          style="width: 40px; margin-top: 3px; margin-left: -10px;"
+        />
+      </div>
+      <div class="user-display">
+        <div class="notifications">
+          <img
+            data-v-4279a8fa
+            src="https://app.incento.io/_nuxt/img/5d8e998.svg"
+            style="width: 40px;"
+          />
+        </div>
+        <div class="profile">
+          <img
+            data-v-126af358
+            style="width: 40px;"
+            src="https://app.incento.io/_nuxt/img/13533ce.svg"
+          />
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -18,6 +41,8 @@
 }
 #nav {
   padding: 30px;
+  display: flex;
+  justify-content: space-between;
 }
 
 #nav a {
@@ -28,4 +53,18 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+.user-display {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.profile {
+  margin-right: -20px;
+  margin-left: 20px;
+}
 </style>
+
+<style scoped>
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="geetings">
+      <div class="greeting-div">
+        <div class="columns is-mobile">
+          <div class="column has-text-weight-bold">
+            Hi,
+            <span class="username">Machu</span>
+          </div>
+          <div class="add-money-btn column has-text-right">
+            <a href="/deposit" class="add-money-link">+ Add Funds</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
+
+<style scoped>
+.greeting-div,
+.services {
+  margin-top: 10%;
+}
+.columns {
+  margin-left: -0.75rem;
+  margin-right: -0.75rem;
+  margin-top: -0.75rem;
+  display: flex;
+  justify-content: space-between;
+}
+.columns:last-child {
+  margin-bottom: -0.75rem;
+}
+.column {
+  display: block;
+  flex-basis: 0;
+  flex-grow: 1;
+  flex-shrink: 1;
+  padding: 0.75rem;
+}
+.has-text-weight-bold {
+  font-weight: 700 !important;
+}
+.add-money-btn {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+    text-align: center;
+    color: #007bff;
+}
+.add-money-link[data-v-00f45f4e] {
+    padding: 5px 10px;
+    border: 1px solid #007bff;
+    border-radius: 50px;
+    background: #f1f0f0;
+}
+.add-money-btn a[data-v-00f45f4e] {
+    color: #007bff;
+}
+a[data-v-00f45f4e] {
+    color: #007bff;
+    cursor: pointer;
+    text-decoration: none;
+}
+</style>
+
